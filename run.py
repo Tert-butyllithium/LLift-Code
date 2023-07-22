63aa7fbe829329e600c3f452f8af3d30c21506f8
Haonan <lihaonan1105@gmail.com>
2023-07-22 10:45:04 -0700

    fix bug: skip stable ones

--- run.py
+++ run.py
@@ -98,7 +98,7 @@ def preprocess_and_analyze(group, max_id, min_id, offset, max_number, model, max
                 
                 # # we allow small inconsistency between preprocessing results
                 if sampling_res:
-                    if max_round != INF and max_round >= 2 and sampling_res.stable == True:
+                    if max_round != INF and case.last_round  >= 2 and sampling_res.stable == True:
                         logging.info(f"Skip analysis for function {case.function}, variable {case.var_name}...")
                         continue 
                 else:
