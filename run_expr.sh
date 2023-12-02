aa4d3ff063044e3a650d631eea4a1d8e79447485
Haonan Li <lihaonan1105@gmail.com>
2023-12-02 20:54:17 +0000

    expr for llama2

--- run_expr.sh
+++ run_expr.sh
@@ -1,3 +1,11 @@
-python3 run.py --group 3 --max_round 3 --max_id -399
-python3 run.py --group 3 --max_round 3 --max_id -399
-python3 run.py --group 3 --max_round 3 --max_id -399
+model=meta-llama/Llama-2-70b-chat-hf
+export SIMPLE_MODE=1
+python3 run.py --group 3 --max_round 100 --model ${model} --max_id -399
+python3 run.py --group 3 --max_round 100 --model ${model} --max_id -399
+python3 run.py --group 3 --max_round 100 --model ${model} --max_id -399
+unset SIMPLE_MODE
+export SELF_VALIDATE=1 
+export ENABLE_PP=1
+python3 run.py --group 3 --max_round 100 --model ${model} --max_id -399
+python3 run.py --group 3 --max_round 100 --model ${model} --max_id -399
+python3 run.py --group 3 --max_round 100 --model ${model} --max_id -399
